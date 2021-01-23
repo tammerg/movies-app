d3b01dbd00b3eb98a88c77c40ea71844be01672f
Tammer Galal <tammergalal20@gmail.com>
2021-01-23 08:07:43 -0500

    working on CORS

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://localhost:5000/api',
+    baseURL: '/api',
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
