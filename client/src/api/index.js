bb48b80acf34a37a7706ad4415de8085f5a35061
Tammer Galal <tammergalal20@gmail.com>
2021-01-23 08:24:00 -0500

    trying to fix CORS

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'https://localhost:5000/api',
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
