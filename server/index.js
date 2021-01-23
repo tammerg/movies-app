7d41c5d59dbf4aa3ccf0eb1f02c189faeda093d7
Tammer Galal <tammergalal20@gmail.com>
2021-01-23 00:54:57 -0500

    trying to fix cors

--- server/index.js
+++ server/index.js
@@ -19,7 +19,6 @@ app.get('/', (req, res) => {
 })
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5000/api"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
