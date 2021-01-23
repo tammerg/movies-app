24997ea945844e31547e48770e583b0b4630c674
Tammer Galal <tammergalal20@gmail.com>
2021-01-23 08:38:05 -0500

    trying to fix issue

--- server/index.js
+++ server/index.js
@@ -16,8 +16,13 @@ app.use(bodyParser.json());
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, GET');
+    return res.status(200).json({})
+  }
   next();
 });
 
