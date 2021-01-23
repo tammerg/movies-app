572ef03f98020972f6eb0ec761db9d77b77d09f5
Tammer Galal <tammergalal20@gmail.com>
2021-01-23 01:23:08 -0500

    working on CORS issue

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const cors_proxy = require('cors-anywhere');
 
 const db = require('./db')
 const movieRouter = require('./routes/movie-router')
@@ -20,6 +21,12 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use('/api', movieRouter)
+app.use('/api', movieRouter);
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+cors_proxy.createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2']
+}).listen(apiPort, host, function() {
+  console.log('Running CORS Anywhere on ' + host + ':' + apiPort);
+});
