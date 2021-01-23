9482c7b5871339fe87a5951f801d83110bf23a0a
Tammer Galal <tammergalal20@gmail.com>
2021-01-23 01:44:32 -0500

    trying to fix CORS issue

--- server/index.js
+++ server/index.js
@@ -23,10 +23,4 @@ app.use(function(req, res, next) {
 });
 app.use('/api', movieRouter);
 
-cors_proxy.createServer({
-  originWhitelist: [], // Allow all origins
-  requireHeader: ['origin', 'x-requested-with'],
-  removeHeaders: ['cookie', 'cookie2']
-}).listen(apiPort, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + apiPort);
-});
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
